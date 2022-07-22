f11a3ae8424e5f4610978190566c10f796a7a16b
Lester Deeley <lester.deeley@yahoo.com>
2022-07-22 16:37:25 +0100

    tests for Functional approach

---
+++ src/main/java/org/example/stringnumbermaths/StringToInt.java
@@ -0,0 +1,13 @@
+package org.example.stringnumbermaths;
+
+import java.security.SecureRandom;
+import java.util.Map;
+
+public class StringToInt {
+
+    public static Integer stringToInt(String string){
+        // Convert a String to its int equivalent
+        return Integer.parseInt(string);
+    }
+
+}
---
+++ src/test/java/org/example/stringnumbermaths/StringToIntTest.java
@@ -0,0 +1,15 @@
+package org.example.stringnumbermaths;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringToIntTest {
+
+    @Test
+    void stringToInt() {
+
+        assertEquals(StringToInt.stringToInt("505"),505);
+
+    }
+}
